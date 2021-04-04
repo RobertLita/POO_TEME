5d5d641dbd4ee93204ecf89d167aab3131f2f29b
RobertLita <74457963+RobertLita@users.noreply.github.com>
2021-04-04 22:01:39 +0300

    Add files via upload

---
+++ POO_TEMA2/cmake-build-debug/CMakeFiles/POO_TEMA2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POO_TEMA2.dir/main.cpp.obj"
+  "POO_TEMA2.exe"
+  "POO_TEMA2.exe.manifest"
+  "POO_TEMA2.pdb"
+  "libPOO_TEMA2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POO_TEMA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
